b7e47ec948ccfede6561ccdde64de686b9875a34
TeacherSon27 <TeacherSon27@users.noreply.github.com>
2026-07-31 05:52:40 +0800

    Activate interface updates immediately

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "whats-wrong-v38";
+const CACHE_NAME = "whats-wrong-v39";
 const ASSETS = [
   "./",
   "./index.html",
@@ -46,14 +46,19 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
